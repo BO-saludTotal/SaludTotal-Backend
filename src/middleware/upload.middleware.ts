5b891524eaaa0f1ad4e466ab005936a8ec186a7f
sis <sis@localhost.localdomain>
2025-05-13 20:07:39 -0400

    API

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -2,7 +2,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-// Configuración de almacenamiento para Multer
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(process.cwd(), 'uploads', 'verificaciones');
@@ -18,23 +18,23 @@ const storage = multer.diskStorage({
   }
 });
 
-// Filtro de archivos
+
 const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-  const allowedMimes = ['image/jpeg', 'image/png', 'application/pdf'];
+  const allowedMimes = ['image/jpeg', 'image/png', 'application/pdf',  'application/zip','application/x-rar-compressed'];
   if (allowedMimes.includes(file.mimetype)) {
     cb(null, true);
   } else {
     const err = new Error('Tipo de archivo no permitido. Solo se aceptan imágenes (JPEG, PNG) y PDF.') as any;
-    err.status = 400; // Puedes añadir un status para manejarlo mejor luego
+    err.status = 400; 
     cb(err, false);
   }
 };
 
-// Configuración de Multer
+
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 5 // Límite de tamaño del archivo (ej: 5MB)
+    fileSize: 1024 * 1024 * 5 //5 megas de tam 1024 bytes * 1024 kb * 5 mb
   },
   fileFilter: fileFilter
 });
