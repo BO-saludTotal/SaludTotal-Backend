8dc661f76c8b322b309265117865ce8f402b2636
JMjuanma <juanma.soruco@gmail.com>
2025-05-14 22:30:23 -0400

    nestjs

--- src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { crearUsuario } from '../controllers/user.controller';
-
-const router = Router();
-
-router.post('/usuarios', crearUsuario);
-
-export default router;
-
