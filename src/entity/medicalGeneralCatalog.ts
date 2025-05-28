c2319a324d0f0a31df742120c6e14ecc3b73ef6c
JMjuanma <juanma.soruco@gmail.com>
2025-05-27 22:07:04 -0400

    patata

--- src/entity/medicalGeneralCatalog.ts
+++ src/entity/medicalGeneralCatalog.ts
@@ -1,22 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
 
-import { Entity, 
-    Column, 
-    PrimaryGeneratedColumn, 
-    Index 
-} from "typeorm";
-
-@Entity({ name: "MedicamentosCatalogoGeneral" })
-@Index(["activeIngredient", "pharmaceuticalForm", "concentration"], { unique: true })
+@Entity({ name: 'MedicamentosCatalogoGeneral' })
+@Index(['activeIngredient', 'pharmaceuticalForm', 'concentration'], {
+  unique: true,
+})
 export class MedicalGeneralCatalog {
-    @PrimaryGeneratedColumn({ name: "MedicamentoGeneralID" })
-    medicalGeneralId: number;
+  @PrimaryGeneratedColumn({ name: 'MedicamentoGeneralID' })
+  medicalGeneralId: number;
 
-    @Column({ name: "PrincipioActivo", type: "varchar", length: 255 })
-    activeIngredient: string;
+  @Column({ name: 'PrincipioActivo', type: 'varchar', length: 255 })
+  activeIngredient: string;
 
-    @Column({ name: "FormaFarmaceutica", type: "varchar", length: 100 })
-    pharmaceuticalForm: string;
+  @Column({ name: 'FormaFarmaceutica', type: 'varchar', length: 100 })
+  pharmaceuticalForm: string;
 
-    @Column({ name: "Concentracion", type: "varchar", length: 100 })
-    concentration: string;
+  @Column({ name: 'Concentracion', type: 'varchar', length: 100 })
+  concentration: string;
 }
---
+++ src/model/medicalSpecialty-model.ts
@@ -0,0 +1,7 @@
+export interface medicalSpecialityModel {
+  id: number;
+  name: string;
+  description: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
