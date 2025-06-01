4e07ff9123c1d899ada2ed352456769f4b5db6a7
Javier Antezana Vallejos <antezanavallejosjavier@gmail.com>
2025-05-31 23:44:12 -0300

    Configuracion de cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
+
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'; // Importa
 
 async function bootstrap() {
@@ -17,9 +18,21 @@ async function bootstrap() {
     app.useGlobalInterceptors(
       new ClassSerializerInterceptor(app.get(Reflector)),
     );
+    
+    
+    //cors
+    app.enableCors({
+		origin: ['https://saludtotal-frontend.pages.dev'],
+		methods:['GET','POST','PUT','DELETE'],
+		credentials: true,
+		});
+    
 
-    const port = process.env.PORT ?? 5000; // O tu puerto
+    const port = process.env.PORT ?? 5000;
+    
     await app.listen(port);
+    
+    console.log((`Backend is running on: http://localhost:${port}`);
     console.log(`Application is running on: http://localhost:${port}`);
   } catch (error) {
     console.error('Error durante el bootstrap:', error);
