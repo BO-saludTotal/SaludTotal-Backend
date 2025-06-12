cae01e2b8db5ce77ed97e77d65817ee975dc6f4c
EL-14-0 <edsonlamas9741@gmail.com>
2025-06-12 19:16:04 -0400

    po

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): any {
     return {
-      nombre: "Edson y Teos backends",
+      nombre: "Edson y Teoooos backends",
 
     };
   }
